51e5f18e66c88709d6ca82fd0fbba133723cd8de
Felix <felix.duechting@uni-bamberg.de>
2023-05-27 10:23:56 +0200

    added Labels to Web view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -548,7 +548,7 @@ class _ExpandedTrailingActionsState extends State<_ExpandedTrailingActions> {
         children: [
           Row(
             children: [
-              const Text('Brightness'),
+              const Text('Helligkeit'),
               Expanded(child: Container()),
               Switch(
                   value: widget.useLightMode,
@@ -571,12 +571,14 @@ class _ExpandedTrailingActionsState extends State<_ExpandedTrailingActions> {
             ],
           ),
           const Divider(),
+          const Text('Farbe'),
           _ExpandedColorSeedAction(
             handleColorSelect: widget.handleColorSelect,
             colorSelected: widget.colorSelected,
             colorSelectionMethod: widget.colorSelectionMethod,
           ),
           const Divider(),
+          const Text('Schriftgröße'),
           StatefulBuilder(
             builder: (context, state){
               return SfSlider(
@@ -659,56 +661,56 @@ class _ExpandedColorSeedAction extends StatelessWidget {
   }
 }
 
-class _ExpandedImageColorAction extends StatelessWidget {
-  const _ExpandedImageColorAction({
-    required this.handleImageSelect,
-    required this.imageSelected,
-    required this.colorSelectionMethod,
-  });
-
-  final void Function(int) handleImageSelect;
-  final ColorImageProvider imageSelected;
-  final ColorSelectionMethod colorSelectionMethod;
-
-  @override
-  Widget build(BuildContext context) {
-    return ConstrainedBox(
-      constraints: const BoxConstraints(maxHeight: 150.0),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: GridView.count(
-          crossAxisCount: 3,
-          children: List.generate(
-            ColorImageProvider.values.length,
-            (i) => InkWell(
-              borderRadius: BorderRadius.circular(4.0),
-              onTap: () => handleImageSelect(i),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Material(
-                  borderRadius: BorderRadius.circular(4.0),
-                  elevation: imageSelected == ColorImageProvider.values[i] &&
-                          colorSelectionMethod == ColorSelectionMethod.image
-                      ? 3
-                      : 0,
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(4.0),
-                      child: Image(
-                        image: NetworkImage(ColorImageProvider.values[i].url),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class _ExpandedImageColorAction extends StatelessWidget {
+//   const _ExpandedImageColorAction({
+//     required this.handleImageSelect,
+//     required this.imageSelected,
+//     required this.colorSelectionMethod,
+//   });
+//
+//   final void Function(int) handleImageSelect;
+//   final ColorImageProvider imageSelected;
+//   final ColorSelectionMethod colorSelectionMethod;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return ConstrainedBox(
+//       constraints: const BoxConstraints(maxHeight: 150.0),
+//       child: Padding(
+//         padding: const EdgeInsets.symmetric(vertical: 8.0),
+//         child: GridView.count(
+//           crossAxisCount: 3,
+//           children: List.generate(
+//             ColorImageProvider.values.length,
+//             (i) => InkWell(
+//               borderRadius: BorderRadius.circular(4.0),
+//               onTap: () => handleImageSelect(i),
+//               child: Padding(
+//                 padding: const EdgeInsets.all(8.0),
+//                 child: Material(
+//                   borderRadius: BorderRadius.circular(4.0),
+//                   elevation: imageSelected == ColorImageProvider.values[i] &&
+//                           colorSelectionMethod == ColorSelectionMethod.image
+//                       ? 3
+//                       : 0,
+//                   child: Padding(
+//                     padding: const EdgeInsets.all(4.0),
+//                     child: ClipRRect(
+//                       borderRadius: BorderRadius.circular(4.0),
+//                       child: Image(
+//                         image: NetworkImage(ColorImageProvider.values[i].url),
+//                       ),
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class NavigationTransition extends StatefulWidget {
   const NavigationTransition(
