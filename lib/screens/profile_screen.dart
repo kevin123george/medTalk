634f70740b8c92ad3743a39bbb30076eaff96776
Harismustafa-atd <harismustafa.atd@gmail.com>
2023-05-29 01:21:44 +0200

    Profile

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,23 +1,10 @@
-// Copyright 2021 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-
-const Widget divider = SizedBox(height: 10);
-
-// If screen content width is greater or equal to this value, the light and dark
-// color schemes will be displayed in a column. Otherwise, they will
-// be displayed in a row.
-const double narrowScreenWidthThreshold = 400;
 
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({super.key});
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
+  _ProfileScreenState createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
@@ -29,8 +16,90 @@ class _ProfileScreenState extends State<ProfileScreen> {
         .apply(displayColor: Theme.of(context).colorScheme.onSurface);
     return Expanded(
       child: Scaffold(
-
+        body: Center(
+          child: ProfileForm(),
+        ),
       ),
     );
+  }
+}
+
+class ProfileForm extends StatefulWidget {
+  const ProfileForm({Key? key}) : super(key: key);
+
+  @override
+  _ProfileFormState createState() => _ProfileFormState();
+}
+
+class _ProfileFormState extends State<ProfileForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _emailController = TextEditingController();
+  final _addressController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 300,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Ihre Name',
+                hintText: 'Gib deinen Namen ein',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Bitte geben Sie Ihren Namen ein';
+                }
+                return null;
+              },
+            ),
+            SizedBox(height: 10),
+            TextFormField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+                hintText: 'Geben sie ihre E-Mail Adresse ein',
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              height: 120,
+              child: TextField(
+                controller: _addressController,
+                decoration: InputDecoration(
+                  labelText: 'Adresse',
+                  hintText: 'Geben Sie Ihre Adresse ein',
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: 5,
+              ),
+            ),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  print('Name: ${_nameController.text}, Email: ${_emailController.text}, Address: ${_addressController.text}');
+                }
+              },
+              child: Text('Update'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _addressController.dispose();
+    super.dispose();
   }
 }
