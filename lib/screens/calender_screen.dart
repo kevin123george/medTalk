3f97bff2350a6a07f3944d890dbe5b8265fd051e
Abeera Ayesha Riaz <abeera-ayesha.riaz@stud.uni-bamberg.de>
2023-07-12 00:08:17 +0200

    added end date to the calender screen

--- lib/screens/calender_screen.dart
+++ lib/screens/calender_screen.dart
@@ -156,27 +156,52 @@ class _CalenderScreenState extends State<CalenderScreen> {
   void _showDialog(Map<String, String> language, List<String> items) {
     String? _selectedRepeat = language['repeat_none'];
     DateTime _selectedDate = DateTime.now();
+    DateTime _endDate = DateTime.now().add(Duration(days: 1));
     String? _eventName = "";
     String? _eventDescription = "";
 
-    Future<void> _pickTime() async {
+    Future<DateTime> _pickTime(String helpText, DateTime initialTime) async {
       final TimeOfDay? picked = await showTimePicker(
+        helpText: helpText,
         context: context,
-        initialTime: TimeOfDay(hour: _selectedDate.hour, minute: _selectedDate.minute),
+        initialTime: TimeOfDay(hour: initialTime.hour, minute: initialTime.minute),
       );
       if (picked != null) {
-        _selectedDate = DateTime(
-          _selectedDate.year,
-          _selectedDate.month,
-          _selectedDate.day,
+        return DateTime(
+          initialTime.year,
+          initialTime.month,
+          initialTime.day,
           picked.hour,
           picked.minute,
         );
+      } else {
+        return initialTime;
+      }
+    }
+
+    Future<void> _pickEndDate() async {
+      final DateTime? picked = await showDatePicker(
+        helpText: "Select End Date",
+        context: context,
+        initialDate: _endDate,
+        firstDate: _endDate,
+        lastDate: DateTime.now().add(Duration(days: 365)),
+      );
+      if (picked != null) {
+        _endDate = DateTime(
+          picked.year,
+          picked.month,
+          picked.day,
+          _endDate.hour,
+          _endDate.minute,
+        );
+        _endDate = await _pickTime("Select end time", _endDate);
       }
     }
 
     Future<void> _pickDate() async {
       final DateTime? picked = await showDatePicker(
+        helpText: "Select Start Date",
         context: context,
         initialDate: _selectedDate,
         firstDate: DateTime.now().subtract(Duration(days: 365)),
@@ -190,7 +215,9 @@ class _CalenderScreenState extends State<CalenderScreen> {
           _selectedDate.hour,
           _selectedDate.minute,
         );
-        await _pickTime(); // Chain the time picker here
+        _selectedDate = await _pickTime("Select start time", _selectedDate); // Chain the time picker here
+        _endDate = _selectedDate.add(Duration(days: 1));
+        await _pickEndDate();
       }
     }
 
@@ -236,12 +263,27 @@ class _CalenderScreenState extends State<CalenderScreen> {
                     Row(
                       children: [
                         Flexible(
-                          child: Text('Selected DateTime: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(_selectedDate)}'),
+                          child: Text('Start Date: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(_selectedDate)}'),
                         ),
                         IconButton(
                           icon: Icon(Icons.calendar_today),
                           onPressed: () async {
                             await _pickDate();
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: Text('End Date: ${DateFormat('yyyy-MM-dd HH:mm:ss').format(_endDate)}'),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.calendar_today),
+                          onPressed: () async {
+                            await _pickEndDate();
                             setState(() {});
                           },
                         ),
@@ -285,7 +327,7 @@ class _CalenderScreenState extends State<CalenderScreen> {
                             final newScheduler = Schedulers(
                               title: eventTitleController.text,
                               startDateTime: _selectedDate.microsecondsSinceEpoch,
-                              endDateTime: null,
+                              endDateTime: _endDate?.microsecondsSinceEpoch,
                               body: eventDescriptionController.text,
                               repeatType: getRepeatTypeFromString(_selectedRepeat ?? ''),
                               reminderTime: _selectedDate.microsecondsSinceEpoch - 5000,
